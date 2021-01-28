da79ae7ce5d77aa96fd2cab59721224d8a78a5de
Emirhan Şenkal <emirhansenkal1@gmail.com>
2021-01-28 15:17:49 +0300

    Initialize Project

---
+++ Assets/EventTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventTest : MonoBehaviour
+{
+    public delegate void OnClicked();
+
+    public static event OnClicked onLeftClicked;
+    public static event OnClicked onRightClicked;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(onLeftClicked != null)
+                onLeftClicked();
+        }else if (Input.GetMouseButtonDown(1))
+        {
+            if(onRightClicked != null)
+                onRightClicked();
+        }
+    }
+
+    public void PrintRight()
+    {
+        Debug.Log("Right");
+    }
+    
+    public void PrintLeft()
+    {
+        Debug.Log("Left");
+    }
+}
---
+++ Assets/UnityEventTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class UnityEventTest : MonoBehaviour
+{
+    [Serializable] public class OnClicked : UnityEvent{ }
+    [Serializable] public class OnPrint : UnityEvent<int>{ }
+
+    public OnClicked onLeftClicked;
+    public OnClicked onRightClicked;
+
+    public OnPrint onPrint;
+    
+    private void Start()
+    {
+        onRightClicked.AddListener(PrintRight);
+        onPrint.AddListener(PrintValue);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            onLeftClicked.Invoke();
+        }
+        else if(Input.GetMouseButtonDown(1))
+        {
+            onRightClicked.Invoke();
+            onPrint.Invoke(13);
+        }
+    }
+
+    public void PrintValue(int value)
+    {
+        Debug.Log(value);
+    }
+    
+    public void PrintRight()
+    {
+        Debug.Log("Right");
+    }
+    
+    public void PrintLeft()
+    {
+        Debug.Log("Left");
+    }
+}
